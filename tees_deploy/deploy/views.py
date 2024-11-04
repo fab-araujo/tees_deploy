30c7f8526070a6aaeeac56809c044068334d7957
Fabricio Araújo <araujopa@gmail.com>
2024-11-04 16:16:02 -0300

    Update views.py

--- tees_deploy/deploy/views.py
+++ tees_deploy/deploy/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("INDEX")
+    return HttpResponse("INDEX 1")
 
 def update(request):
     return HttpResponse("UPDATE")

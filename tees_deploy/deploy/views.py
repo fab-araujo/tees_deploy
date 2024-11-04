0e26ebdfb6c65070725f5fc33dd40ba963d5370a
Fabricio Araújo <araujopa@gmail.com>
2024-11-04 16:31:03 -0300

    Update views.py

--- tees_deploy/deploy/views.py
+++ tees_deploy/deploy/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
 
 # Create your views here.
 def index(request):
-    return HttpResponse("INDEX 2")
+    return HttpResponse("INDEX 3")
 
 def update(request):
+    os.system("git pull origin main")
     return HttpResponse("UPDATE")
